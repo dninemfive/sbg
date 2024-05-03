340c986a961c7b3b44d0da9b4c3bc031ce6d6884
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-05-03 11:38:40 -0700

    drastically simplify IsBaghChalAdjacentTo

--- Extensions.cs
+++ Extensions.cs
@@ -22,24 +22,15 @@ public static class Extensions
             (0, y)
         ];
     }
-    // all valid diagonal moves either enter or exit these points
-    // ... or they hop over them, technically
-    // (but those are attacks in the logic)
-    public static bool IsBaghChalDiagonalCenter(this Point p)
-        => p is (1, 1) or (3, 1) or (3, 3) or (1, 3);
+    public static int TaxicabDistanceTo(this Point a, Point b)
+        => Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y);
     public static bool IsBaghChalAdjacentTo(this Point a, Point b)
-    {
-        if (a == b)
-            return false;
-        (int ax, int ay) = a;
-        (int bx, int by) = b;
-        int xDiff = Math.Abs(ax - bx), yDiff = Math.Abs(ay - by);
-        if (xDiff is not (0 or 1) || yDiff is not (0 or 1))
-            return false;
-        if (xDiff != yDiff) // one space off in x xor y dimensions
-            return true;
-        return a.IsBaghChalDiagonalCenter() || b.IsBaghChalDiagonalCenter();
-    }
+        => a.TaxicabDistanceTo(b) switch
+        {
+            1 => true,
+            2 => (a.x - a.y).IsEven(),
+            _ => false
+        };
     public static IEnumerable<Point> AllCoordinates<T>(this T[,] array)
     {
         for (int x = 0; x < array.GetLength(0); x++)
@@ -63,4 +54,6 @@ public static class Extensions
     }
     public static string IndentLines(this string s, int n = 1, string tab = "  ")
         => $"{tab.Repeated(n)}{s.Replace("\n", $"\n{tab.Repeated(n)}")}";
+    public static bool IsEven(this int i)
+        => i % 2 == 0;
 }
