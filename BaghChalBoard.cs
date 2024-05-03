cd1cc22a289fc956d4832eea62d24530182fe118
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-05-03 01:54:16 -0700

    action sketch, some other stuff i frogor

--- BaghChalBoard.cs
+++ BaghChalBoard.cs
@@ -12,7 +12,9 @@ public readonly struct BaghChalBoard
     }
     public static implicit operator BaghChalBoard(SpaceState[,] spaces)
         => new(spaces);
-    public static BaghChalBoard InitialState
+    public static implicit operator SpaceState[,](BaghChalBoard board)
+        => (SpaceState[,])board.Spaces.Clone();
+    public static BaghChalBoard InitialBoard
     {
         get
         {
@@ -20,6 +22,41 @@ public readonly struct BaghChalBoard
             return result.With(SpaceState.Wolf, result.Corners());
         }
     }
+    public SpaceState this[Point p]
+        => Spaces[p.x, p.y];
+}
+public readonly struct BaghChalState(BaghChalBoard board, int unplacedSheep, int capturedSheep)
+{
+    public readonly int UnplacedSheep = unplacedSheep,
+                        CapturedSheep = capturedSheep;
+    public readonly BaghChalBoard Board = board;
+    public static BaghChalState InitialState => new(BaghChalBoard.InitialBoard, 20, 0);
+}
+public class BaghChalAction(string name, Func<BaghChalState, BaghChalState?> function)
+{
+    public string Name { get; private set; } = name;
+    public BaghChalState? ApplyTo(BaghChalState state) => function(state);
+    public static BaghChalAction PlaceSheepAt(Point p)
+        => new($"Place sheep at {p}", delegate (BaghChalState state)
+        {
+            if (state.UnplacedSheep < 1
+             || state.CapturedSheep >= 5
+             || state.Board[p] != SpaceState.Empty)
+                return null;
+            return new(state.Board.Spaces.With(SpaceState.Sheep, [p]), state.UnplacedSheep - 1, state.CapturedSheep);
+        });
+    public static BaghChalAction Move(SpaceState player, Point source, Point destination)
+        => new($"Move {player} from {source} to {destination}", delegate (BaghChalState state)
+        {
+            if (player is not SpaceState.Sheep or SpaceState.Wolf
+             || state.Board[source] != player
+             || state.Board[destination] != SpaceState.Empty
+             || !source.IsBaghChalAdjacentTo(destination))
+                return null;
+            return new(state.Board.Spaces.With((source, SpaceState.Empty), (destination, player)), state.UnplacedSheep, state.CapturedSheep);
+        });
+    public static BaghChalAction Capture(Point source, Point destination)
+        => throw new NotImplementedException();
 }
 public static class Extensions
 {
@@ -42,4 +79,6 @@ public static class Extensions
             (0, y)
         ];
     }
+    public static bool IsBaghChalAdjacentTo(this Point a, Point b)
+        => throw new NotImplementedException();
 }
