9389e61cb0d9bb7883b0d87595de1c0dd5224b28
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-05-05 13:27:15 -0700

    merge godot and sbg.examples projects for convenience; change godot project to use .NET 8.0 (rip WASM)

--- examples/_Godot/Root.cs
+++ examples/_Godot/Root.cs
@@ -1,11 +1,12 @@
 using Godot;
-using System;
 
+namespace d9.sbg.examples.godot;
 public partial class Root : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		 
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
