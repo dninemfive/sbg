242d77016dc4ad97462ba83d99b86a483efdb719
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-05-05 11:35:58 -0700

    add IGameAction<T>

--- Extensions.cs
+++ Extensions.cs
@@ -27,4 +27,10 @@ public static class Extensions
     public static string Plural<T>(this string s, T n, string ifPlural = "s")
         where T : INumberBase<T>
         => $"{s}{(n != T.One ? ifPlural : "")}";
+    public static IEnumerable<T> NonNullElements<T>(this IEnumerable<T?> maybeNullElements)
+    {
+        foreach (T? element in maybeNullElements)
+            if (element is T t)
+                yield return t;
+    }
 }
---
+++ generic/IGameAction.cs
@@ -0,0 +1,22 @@
+﻿using d9.sbg;
+
+namespace d9.abg.generic;
+public delegate T StateTransitionFunction<T>(T state);
+public delegate Exception? GameActionValidator<T>(T state);
+public interface IGameAction<T>
+{
+    public string Name { get; }
+    protected StateTransitionFunction<T> TransitionFunction { get; }
+    public IReadOnlyCollection<GameActionValidator<T>> Validators { get; }
+    public virtual bool ValidFor(T state, out IEnumerable<Exception> exceptions)
+    {
+        exceptions = Validators.Select(x => x(state)).NonNullElements();
+        return exceptions.Any();
+    }
+    public virtual T ApplyTo(T state)
+    {
+        if (!ValidFor(state, out IEnumerable<Exception> exceptions))
+            throw new AggregateException(exceptions);
+        return TransitionFunction(state);
+    }
+}
